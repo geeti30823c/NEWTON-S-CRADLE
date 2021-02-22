959d5a4eface5d2bf49eb78bbba7587039cd5bd4
geeti30823c <73381121+geeti30823c@users.noreply.github.com>
2021-02-22 21:07:22 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,36 @@
+class bob
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			//restitution:0.3,
+			restitution:1.09,
+			friction:0,
+			//density:1.2
+			density:0.8
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var bobpos=this.body.position;		
+
+			push()
+			translate(bobpos.x, bobpos.y);
+			rectMode(CENTER)
+			//strokeWeight(3);
+			fill("blue")
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
